0655a96c42fc701a09e26466cc315db93437eefb
Micah Glesener <micahglesener@gmail.com>
2024-07-31 18:34:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function check() {
 	const inEle = document.getElementById('inputElement') ;
+	console.log(inEle) ;
 	const gmBrd = document.getElementById('gameboard')
-	gmBrd.innerHTML = inEle.value ;
+	console.log(gmBrd) ;
+	const inStr = inEle.value ;         // lack () means 'property' (data)
+	console.log(inStr) ;
+	const upStr = inStr.toUpperCase() ; // have () means 'method'   (compute)
+	console.log(upStr) ;
+	gmBrd.innerHTML = upStr ;
 }

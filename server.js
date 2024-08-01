f4dd1866e922c76d5830adb1b82a669a81743609
Micah Glesener <micahglesener@gmail.com>
2024-07-31 18:45:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 
+const http = require('http');
+
+function serverFunc(req, res) {
+  res.write('Hello World!') ; 
+  res.end() ; 
+}
+
+serverObj = http.createServer(serverFunc) ;
+
+serverObj.listen(8080) ;
